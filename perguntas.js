9b467b07013e567d2aa5b0ba01ff75d3539d2183
copemaria21 <132694461+copemaria21@users.noreply.github.com>
2024-10-07 08:10:13 -0300

    07/10/2024 projeto final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'O que foi o regime militar no Brasil?',
+    'O regime militar durou de 64-85, com censura, repressão política e violações dos direitos humanos'
+)
+
+criaCartao(
+    'Química',
+    'Quais os prefixos e sufixos das cadeias carbonicas?',
+    'prefixos: indicam o número de carbonos(met, et, prop, but...) e os sufixos o tipo de ligação(ano, eno, ino)'
+)
+
+criaCartao(
+    'Física',
+    ' Quais são as leis de Newton ?',
+    'primeira lei: inércia; segunda lei: lei da força F= m*a; terceira lei: ação e reação'
+)
+
+criaCartao(
+    'Biologia',
+    'O que diz a teoria de evolução de Darwin?',
+    'A teoria da evolução de Darwin, principalmente através da seleção natural, sugere que as espécies mudam ao longo do tempo. Indivíduos com características vantajosas têm mais chances de sobreviver e reproduzir, transmitindo essas características para a próxima geração. Essa adaptação ao ambiente leva à diversificação das espécies'
+)
